21cd054c9bdebe71b99a643a47f171a7beb918ab
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2020-10-22 16:17:13 +0200

    Prepare 64 bit GDT

--- stage2.asm
+++ stage2.asm
@@ -11,7 +11,7 @@ start:
     mov ss, ax                          ;set stack segment address
     mov sp, 0                           ;0 to stack pointer register
 
-    lgdt [GDT_ADR]                      ;loads GDT to GDTR GDT register
+    lgdt [GDT_ADR32]                    ;loads GDT to GDTR GDT register
                                         ;intel manual 2A -> page 663
 
     mov eax, cr0                        ;set processor flag
@@ -52,13 +52,13 @@ stage32:
 ;segment selector (16-bit identifier for a segment) -> page 95
 
 ;GLOBAL DESCRIPTOR TABLE 32-BIT
-GDT:
+GDT32:
 
 ; Null segment
 dd 0, 0
 
 ; Code segment -> page 97
-dd 0xffff                               ;2Bytes base address, 2Bytes segment limit(size)
+dd 0xffff                                   ;2Bytes base address, 2Bytes segment limit(size)
 ;BA - bits [7:0] - base address
 ;Type - bits [11:8] - read/execution -> 0b1010 -> Table 3.1 page 100
 ;S - bit 12 - the segment descriptor is for a system segment (S flag is clear) or a code or data segment (S flag is set)
@@ -72,20 +72,59 @@ dd 0xffff                               ;2Bytes base address, 2Bytes segment lim
 dd (10 << 8) | (1 << 12) | (1 << 15) | (15 << 16) | (1 << 22) | (1 << 23)
 
 ; Data segment
-dd 0xffff                               ;2Bytes base address, 2Bytes segment limit(size)
+dd 0xffff                                   ;2Bytes base address, 2Bytes segment limit(size)
 dd (2 << 8) | (1 << 12) | (1 << 15) | (15 << 16) | (1 << 22) | (1 << 23)
 
 ; Its helpfull when number of segment is multiply of 2
 dd 0, 0
 
-GDT_END:
+GDT_END32:
 
 ;this is GDT address structure for lgdt instruction
 ;intel manuals 2A -> page 663
 ;2Bytes limit/size, 4Bytes Address
-GDT_ADR:
-dw (GDT_END - GDT) - 1                  ;size of GDT above as 2Bytes
-dd 0x10000 + GDT                        ;16 bit mode address GDT
+GDT_ADR32:
+dw (GDT_END32 - GDT32) - 1                  ;size of GDT above as 2Bytes
+dd 0x10000 + GDT32                          ;16 bit mode address GDT
 times (32 - ($ - $$) % 32) dd 0xcc
 
-times 1024 - ($ - $$) db 0
+;GLOBAL DESCRIPTOR TABLE 64-BIT
+;L -> 1
+;D/B -> 0
+GDT64:
+
+; Null segment
+dd 0, 0
+
+; Code segment -> page 97
+dd 0xffff                                   ;2Bytes base address, 2Bytes segment limit(size)
+;BA - bits [7:0] - base address
+;Type - bits [11:8] - read/execution -> 0b1010 -> Table 3.1 page 100
+;S - bit 12 - the segment descriptor is for a system segment (S flag is clear) or a code or data segment (S flag is set)
+;DPL - bits[13:14] - DPL (descriptor privilege level) field specifies the privilege level of the segment -> 0 to 3
+;P - bit 15 - indicates whether the segment is present in memory (set) or not present (clear)
+;SegLimit - bits [19:16] - segment limit is 0xffff so here goes 0xf
+;AVL - bit 20 - Available for use by system software
+;L - bit 21 - 1 64 bit segment, 0 no 64 bits segment
+;D/B - bit 22 - 0 16 bit segment, 1 32 bits segment
+;G - bit 23 - Granularity -  granularity flag is clear, the segment limit is interpreted in byte units; when flag is set, the segment limit is interpreted in 4-KByte units.
+dd (10 << 8) | (1 << 12) | (1 << 15) | (15 << 16) | (1 << 21) | (1 << 23)
+
+; Data segment
+dd 0xffff                                   ;2Bytes base address, 2Bytes segment limit(size)
+dd (2 << 8) | (1 << 12) | (1 << 15) | (15 << 16) | (1 << 21) | (1 << 23)
+
+; Its helpfull when number of segment is multiply of 2
+dd 0, 0
+
+GDT_END64:
+
+;this is GDT address structure for lgdt instruction
+;intel manuals 2A -> page 663
+;2Bytes limit/size, 4Bytes Address
+GDT_ADR64:
+dw (GDT_END64 - GDT64) - 1                  ;size of GDT above as 2Bytes
+dd 0x10000 + GDT64                          ;16 bit mode address GDT
+times (32 - ($ - $$) % 32) dd 0xcc
+
+times 2048 - ($ - $$) db 0
